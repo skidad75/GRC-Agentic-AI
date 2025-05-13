b2b3a9e66e85ad9c7c045eb175c80455b35bc3f5
Ryan S <skidad75@gmail.com>
2025-05-13 15:36:49 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,7 @@ if project_root not in sys.path:
 from app.agent_router import route_query
 
 # LlamaIndex imports
-from llama_index.core import VectorStoreIndex
-from llama_index.readers.file import SimpleDirectoryReader
-from llama_index.llms.openai import OpenAI
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, OpenAI
 
 # Constants
 MAX_SEARCHES = 100
