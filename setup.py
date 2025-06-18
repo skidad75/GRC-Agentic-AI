2899a03ab1c248a62356f6297b83de3ebba5b5af
Ryan S <skidad75@gmail.com>
2025-06-18 12:11:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-install_requires=[
-    "streamlit==1.45.0",
-    "openai==1.12.0",
-    "python-dotenv==1.0.1",
-    "requests==2.31.0",
-    "beautifulsoup4>=4.12.3,<5.0.0",
-    "tiktoken==0.5.2",
-    "plotly==5.19.0",
-    "altair==5.2.0",
-    "streamlit-extras==0.6.0",
-    "streamlit-option-menu==0.4.0",
-    "streamlit-chat==0.1.1",
-    "streamlit-ace==0.1.1",
-    "streamlit-autorefresh==1.0.1",
-    "pypdf"
-]
+    name="grc-agentic-ai",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "streamlit==1.45.0",
+        "openai==1.12.0",
+        "python-dotenv==1.0.1",
+        "requests==2.31.0",
+        "beautifulsoup4>=4.12.3,<5.0.0",
+        "tiktoken==0.5.2",
+        "plotly==5.19.0",
+        "altair==5.2.0",
+        "streamlit-extras==0.6.0",
+        "streamlit-option-menu==0.4.0",
+        "streamlit-chat==0.1.1",
+        "streamlit-ace==0.1.1",
+        "streamlit-autorefresh==1.0.1",
+        "pypdf"
+    ],    
 )
